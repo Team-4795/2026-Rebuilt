f84bf4bcc3c1343601df0a710c92e5b2b1279a2e
cathys <dragonthebee@gmail.com>
2026-01-15 16:30:28 -0500

    fixed deprecated stuff

--- src/main/java/frc/robot/subsystems/vision/VisionIOReal.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOReal.java
@@ -1,30 +1,30 @@
 package frc.robot.subsystems.vision;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose3d;
+
 import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.PhotonTargetSortMode;
 import org.photonvision.targeting.PhotonPipelineResult;
 
 public class VisionIOReal implements VisionIO {
   PhotonCamera camera;
   PhotonPoseEstimator estimator;
-
   PhotonTargetSortMode sortMode;
   List<PhotonPipelineResult> result;
 
   public VisionIOReal(int camIndex) {
-    camera = new PhotonCamera(VisionConstants.CAM_NAMES[camIndex]);
+  camera = new PhotonCamera(VisionConstants.CAM_NAMES[camIndex]);
 
-    estimator =
-        new PhotonPoseEstimator(
-            AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField),
-            VisionConstants.CAM_POSES[camIndex]);
-    estimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+  estimator =
+    new PhotonPoseEstimator(
+    VisionConstants.FIELD_LAYOUT,
+    VisionConstants.CAM_POSES[camIndex]);
+
     sortMode = PhotonTargetSortMode.Largest;
   }
 
@@ -33,32 +33,31 @@ public class VisionIOReal implements VisionIO {
     inputs.pipelineIndex = camera.getPipelineIndex();
     inputs.sortMode = sortMode.toString();
 
-    result = camera.getAllUnreadResults();
+    Optional<EstimatedRobotPose> visionEst = Optional.empty();
+    for (var result : camera.getAllUnreadResults()) {
+        visionEst = estimator.estimateCoprocMultiTagPose(result); // Multitag estimation
+        if (visionEst.isEmpty()) {
+            visionEst = estimator.estimateLowestAmbiguityPose(result); // Singletag if only one tag is in view
+        }
 
-    for (int i = 0; i < result.size(); i++) {
-      if (result.get(i).getBestTarget() != null) {
-        inputs.poseAmbiguity = result.get(i).getBestTarget().getPoseAmbiguity();
-      }
-
-      estimator
-          .update(result.get(i), camera.getCameraMatrix(), camera.getDistCoeffs())
-          .ifPresentOrElse(
-              (pose) -> {
+        // Update logs if we're getting vision data
+        visionEst.ifPresentOrElse(
+            (pose) -> {
                 inputs.pose = new Pose3d[] {pose.estimatedPose};
                 inputs.timestamp = new double[] {pose.timestampSeconds};
                 inputs.tags =
                     pose.targetsUsed.stream()
                         .mapToInt(
                             (target) -> {
-                              return target.getFiducialId();
+                                return target.getFiducialId();
                             })
                         .toArray();
-              },
-              () -> {
+            },
+            () -> {
                 inputs.pose = new Pose3d[] {};
                 inputs.timestamp = new double[] {};
                 inputs.tags = new int[] {};
-              });
+            });
     }
   }
 }
