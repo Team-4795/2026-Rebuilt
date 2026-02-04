d502bde13fcb64c36960f96e707db5434146ea76
NityaSharma24 <nsharma1@students.chccs.k12.nc.us>
2026-02-04 17:34:13 -0500

    start on button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -57,7 +57,13 @@ public class RobotContainer {
    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_operatorController 
+    .a()
+    .whileTrue(
+      Commands.runOnce()
+    );
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
