d90019c07b1cae3b1c7d7b439cb1087ad29480c6
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2026-02-11 11:30:52 -0500

    climb started

---
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.climb;
+
+public class ClimbConstants {
+    public static int canID = 15; //placeholder
+    public static int current = 60;
+}
---
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOReal.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems.climb;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+public class ClimbIOReal implements ClimbIO{
+    SparkFlex climb = new SparkFlex(ClimbConstants.canID, MotorType.kBrushless);
+    RelativeEncoder encoder = climb.getEncoder(); 
+    double voltage = 0;
+
+    SparkFlexConfig config = new SparkFlexConfig(); 
+    
+    public ClimbIOReal() {
+        config.smartCurrentLimit(ClimbConstants.current);
+        config.idleMode(IdleMode.kBrake);
+        climb.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        climb.clearFaults();
+    }
+
+    @Override
+    public void setVoltage(double volts) {
+        voltage = volts; 
+        climb.setVoltage(volts);
+    }
+
+    @Override
+    public void updateInputs(ClimbIOInputs inputs) {
+        inputs.velocity = encoder.getVelocity();
+        inputs.current = climb.getAppliedOutput(); 
+        inputs.volts = voltage; 
+    }
+
+
+}
