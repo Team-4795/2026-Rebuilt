2cb1645aca521321be2417fd1f41d3c795466c60
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2026-02-17 16:00:02 -0500

    intake pwm stuff

--- src/main/java/frc/robot/subsystems/Intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOReal.java
@@ -1,45 +1,16 @@
 package frc.robot.subsystems.Intake;
 
-import com.revrobotics.PersistMode;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkFlex;
 
 public class IntakeIOReal implements IntakeIO {
   // one motors on each side of the spinny bar thing
-  private final SparkFlex intakeMotorA =
-      new SparkFlex(IntakeConstants.canIDIntakeA, MotorType.kBrushless);
-  private final SparkFlex intakeMotorB =
-      new SparkFlex(IntakeConstants.canIDIntakeB, MotorType.kBrushless);
-
-  private SparkFlexConfig intakeConfigA = new SparkFlexConfig();
-  private SparkFlexConfig intakeConfigB = new SparkFlexConfig();
+  private final PWMSparkFlex intakeMotorA = new PWMSparkFlex(IntakeConstants.PWMPort);
 
   private double intakeVolts = 0;
 
   public IntakeIOReal() {
     // Intake motor config
-    intakeMotorA.clearFaults();
-    intakeMotorB.clearFaults();
-
-    intakeConfigA.smartCurrentLimit(IntakeConstants.CURRENT_LIMIT);
-    intakeConfigA.idleMode(IdleMode.kCoast);
-
-    intakeConfigA.voltageCompensation(12.0);
-    intakeConfigA.inverted(false);
-
-    intakeConfigB.apply(intakeConfigA);
-    intakeConfigB.follow(intakeMotorA.getDeviceId(), true);
-
-    intakeMotorA.configure(
-        intakeConfigA, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    intakeMotorB.configure(
-        intakeConfigB, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    intakeMotorA.setCANTimeout(20);
-    intakeMotorB.setCANTimeout(20);
+    intakeMotorA.setSafetyEnabled(true);
   }
 
   @Override
@@ -50,12 +21,6 @@ public class IntakeIOReal implements IntakeIO {
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
-    inputs.voltageA = intakeVolts;
-    inputs.angularVelocityRPSA = intakeMotorA.getEncoder().getVelocity();
-    inputs.currentAmpsA = intakeMotorA.getOutputCurrent();
-
-    inputs.voltageB = intakeVolts;
-    inputs.angularVelocityRPSB = intakeMotorB.getEncoder().getVelocity();
-    inputs.currentAmpsB = intakeMotorB.getOutputCurrent();
+    inputs.voltageA = intakeMotorA.getVoltage();
   }
 }
