37847dcdcb62abdc21bc68a5197324cfc16f75fd
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2026-03-03 12:53:30 -0500

    intake changes

--- src/main/java/frc/robot/subsystems/Intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOReal.java
@@ -1,22 +1,26 @@
 package frc.robot.subsystems.Intake;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkFlex;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
 
 public class IntakeIOReal implements IntakeIO {
   // one motors on each side of the spinny bar thing
-  private final PWMSparkFlex intakeMotorA = new PWMSparkFlex(IntakeConstants.PWMPort);
+  private final PWMTalonFX intakeMotorA = new PWMTalonFX(IntakeConstants.PWMPort);
+  private final PWMTalonFX intakeMotor2 = new PWMTalonFX(IntakeConstants.PWMPort2);
 
   private double intakeVolts = 0;
 
   public IntakeIOReal() {
     // Intake motor config
-    intakeMotorA.setSafetyEnabled(true);
+    intakeMotor2.setInverted(true);
+    intakeMotorA.setInverted(false);
   }
 
   @Override
   public void setIntakeVoltage(double v) {
     intakeVolts = v;
     intakeMotorA.setVoltage(intakeVolts);
+    intakeMotor2.setVoltage(intakeVolts);
   }
 
   @Override
